c9877fcf62b97915a3583a6f0b6956e23f6ccc26
JuanJo <135390925+varquhaz@users.noreply.github.com>
2024-12-14 23:05:26 -0400

    SETUP - Hilt in application

---
+++ app/src/main/java/com/varqulabs/compressorbitwise/CompressorBitwiseApplication.kt
@@ -0,0 +1,9 @@
+package com.varqulabs.compressorbitwise
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CompressorBitwiseApplication : Application() {
+
+}
